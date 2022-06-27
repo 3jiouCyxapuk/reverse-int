6a3e508763002967f55b73f3ebbf3561038ac803
3jioucyxapuk <serega.sid@gmail.com>
2022-06-27 08:33:29 +0300

    feat: add revert function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = '';
+    let nString = n.toString();
+for (let i = nString.length - 1; i >=0; i--)
+    {result = `${result}${nString[i]}`}
+    return parseInt(result);
+};
