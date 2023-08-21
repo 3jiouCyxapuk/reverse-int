b7c8683bf4d3630ca4d471eff2d113ece64375ff
Sergey Drobyshevskiy <serega.sid@gmail.com>
2023-08-21 23:05:13 +0300

    added new solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 module.exports = function reverse(number) {
     let stringifiedNumber = String(Math.abs(number));
+    let result = stringifiedNumber.split('').reverse().join('')
+    return Number(result);
+};
+
+/* Old solution
+module.exports = function reverse(number) {
+    let stringifiedNumber = String(Math.abs(number));
     let result = "";
 
     for (let i = stringifiedNumber.length - 1; i >= 0; i--) {
@@ -7,3 +14,4 @@ module.exports = function reverse(number) {
     }
     return +result;
 };
+*/
