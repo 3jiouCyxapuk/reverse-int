54e512c55e7e0f802f9a1588814bea9f29a9d87f
Sergey Drobyshevskiy <serega.sid@gmail.com>
2023-08-21 22:52:26 +0300

    fix and refactor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-module.exports = function reverse (n) {
-    let result = '';
-    let nString = n.toString();
-for (let i = nString.length - 1; i >=0; i--)
-    {result = `${result}${nString[i]}`}
-    return parseInt(result);
+module.exports = function reverse(number) {
+    let stringNumber = String(Math.abs(number));
+    let result = "";
+
+    for (let i = stringNumber.length - 1; i >= 0; i--) {
+        result += stringNumber[i];
+    }
+    return +result;
 };
